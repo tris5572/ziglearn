6110b681972e0502474c6d659aeca1706916446d
tris <68403260+tris5572@users.noreply.github.com>
2025-12-12 21:17:59 +0900

    feat: While loops

---
+++ while_loops.zig
@@ -0,0 +1,41 @@
+const expect = @import("std").testing.expect;
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+
+// for 文っぽく、continue （次のループ突入時）で実行される式を、ループ条件の横に書ける
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+    try expect(i == 11);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4); // 0 + 1 + 3
+    try expect(i == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
+}
